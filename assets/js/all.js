0958e0f6cdd2b2018a6b55ec8a2921f640d49f0d
chris chen <christinachen149@gmail.com>
2022-08-11 11:05:01 +0800

    Update 2022-08-11T03:04:58.344Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,9 @@
 "use strict";
 
-$('.ham-icon').onclick(function (e) {
-  e.preventDefault();
-  $('.dropdown').toggleClass('active');
+jQuery(function () {
+  $('.ham-icon').on('click', function (e) {
+    e.preventDefault();
+    $('.dropdown').toggleClass('active');
+  });
 });
 //# sourceMappingURL=all.js.map
