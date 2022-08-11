7bdb39504e68199ae7a2b24d1ef715dec1b4da0c
chris chen <christinachen149@gmail.com>
2022-08-11 11:03:58 +0800

    Fixed mobile menu  dropdown

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,6 @@
-console.log('Hello!');
-
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
-});
+jQuery(function() {
+  $('.ham-icon').on( 'click', function(e) {
+    e.preventDefault();
+    $('.dropdown').toggleClass('active')
+  })
+})
